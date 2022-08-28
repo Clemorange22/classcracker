0d6ef5de9c56c0291d50dd8dbfdb47752710bb0d
Clément Boittin <clement.boittin@proton.me>
2022-08-28 22:16:52 +0200

    Correct ts errors

--- src/api/database/factories/StudentFactory.ts
+++
@@ -1,8 +0,0 @@
-import Student from 'App/Models/Student'
-import Factory from '@ioc:Adonis/Lucid/Factory'
-
-export default Factory.define(User, ({ faker }) => {
-  return {
-    //
-  }
-}).build()
